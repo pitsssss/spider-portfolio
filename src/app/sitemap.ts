b25762bef55b2ce5425676682f06b868f0a8cc0e
Peter Toss <frontendtamkeen@gmail.com>
2026-07-26 13:51:54 +0300

    Fix: Add force-static export to robots and sitemap for static build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,7 @@
 import type { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [{ url: 'https://peterparker.dev', lastModified: new Date(), changeFrequency: 'monthly', priority: 1 }]
 }
